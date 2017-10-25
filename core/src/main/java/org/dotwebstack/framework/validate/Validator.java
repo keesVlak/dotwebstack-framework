0a3047cc4aa567d68e4b26717a5d945b3e3c8c92
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-10-25 14:52:38 +0200

    Change validator interface

--- core/src/main/java/org/dotwebstack/framework/validate/Validator.java
+++ core/src/main/java/org/dotwebstack/framework/validate/Validator.java
@@ -1,7 +1,9 @@
 package org.dotwebstack.framework.validate;
 
-public interface Validator<R> {
+public interface Validator<R, M> {
 
   void validate(R data, R shapes) throws SHACLValdiationException;
+
+  void getValidationReport(M reportModel) throws SHACLValdiationException;
 
 }
