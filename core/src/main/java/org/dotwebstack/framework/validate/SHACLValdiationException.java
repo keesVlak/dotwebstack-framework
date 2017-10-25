64afe70aabac08d06b10dbc76c22d9fd7725a55d
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-10-25 14:35:36 +0200

    Remove unused exception methods

--- core/src/main/java/org/dotwebstack/framework/validate/SHACLValdiationException.java
+++ core/src/main/java/org/dotwebstack/framework/validate/SHACLValdiationException.java
@@ -4,20 +4,12 @@ public class SHACLValdiationException extends Exception {
 
   private static final long serialVersionUID = 6908579800668544739L;
 
-  public SHACLValdiationException() {
-    super();
-  }
-
   public SHACLValdiationException(String message) {
     super(message);
   }
 
   public SHACLValdiationException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  public SHACLValdiationException(Throwable cause) {
-    super(cause);
   }
 
 }
